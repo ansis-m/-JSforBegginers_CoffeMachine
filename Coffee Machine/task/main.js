bc269023cfe5e1b6e87003ec2dc48aeb78af647f
Ansis Maleckis <maleckisansis@gmail.com>
2022-09-24 12:00:33 +0300

    initialized the project

---
+++ Coffee Machine/task/main.js
@@ -0,0 +1,15 @@
+
+const input = require('sync-input');
+
+function main() {
+    console.log("Starting to make a coffee\n" +
+        "Grinding coffee beans\n" +
+        "Boiling water\n" +
+        "Mixing boiled water with crushed coffee beans\n" +
+        "Pouring coffee into the cup\n" +
+        "Pouring some milk into the cup\n" +
+        "Coffee is ready!");
+}
+
+main();
+

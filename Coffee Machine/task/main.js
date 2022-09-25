dfa35502aec0b58a61b374b0b9e33a1b9a738d6a
Ansis Maleckis <maleckisansis@gmail.com>
2022-09-25 10:55:20 +0300

    completed stage 3

--- Coffee Machine/task/main.js
+++ Coffee Machine/task/main.js
@@ -2,6 +2,17 @@
 const input = require('sync-input');
 
 function main() {
+
+    console.log("Write how many ml of water the coffee machine has:");
+    let water = Number(input());
+
+    console.log("Write how many ml of milk the coffee machine has:");
+    let milk = Number(input());
+
+    console.log("Write how many grams of coffee beans the coffee machine has:");
+    let beans = Number(input());
+
+
     console.log("Write how many cups of coffee you will need:");
 
     let amount = new Number();
@@ -14,8 +25,15 @@ function main() {
             break;
     }
 
-    console.log(`For ${amount} cups of coffee you will need:`)
-    console.log(`${amount * 200} ml of water\n${amount * 50} ml of milk\n${amount * 15} g of coffee beans`)
+
+    let possibleCups = Math.min(~~(water/200), ~~(milk/50), ~~(beans/15));
+
+    if (amount > possibleCups)
+        console.log(`No, I can make only ${possibleCups} cups of coffee`);
+    else if (amount == possibleCups)
+        console.log(`Yes, I can make that amount of coffee`);
+    else if (amount < possibleCups)
+        console.log(`Yes, I can make that amount of coffee (and even ${possibleCups - amount} more than that)`);
 
 }
 

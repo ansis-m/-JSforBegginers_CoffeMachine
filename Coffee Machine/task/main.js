ae396a927bdc1f84044aeb60765791b9318c34b0
Ansis Maleckis <maleckisansis@gmail.com>
2022-09-25 12:19:38 +0300

    last stage

--- Coffee Machine/task/main.js
+++ Coffee Machine/task/main.js
@@ -7,6 +7,10 @@ let beans = 120;
 let cups = 9;
 let money = 550;
 
+const espresso = [250, 0, 16, 4];
+const latte = [350, 75, 20, 7];
+const cappuccino = [200, 100, 12, 6];
+
 function printStatus(){
     console.log("The coffee machine has:\n" +
         `${water} ml of water\n` +
@@ -16,64 +20,39 @@ function printStatus(){
         `$${money} of money`);
 }
 
+function make(coffee){
+    if (!(water >= coffee[0] && milk >= coffee[1] && beans >= coffee[2] && cups > 0)){
+        if(water < coffee[0])
+            console.log("Sorry, not enough water!");
+        if(milk < coffee[1])
+            console.log("Sorry, not enough milk!");
+        else if(beans < coffee[2])
+            console.log("Sorry, not enough beans!");
+        else if(cups < 1)
+            console.log("Sorry, not enough cups!");
+        return;
+    }
+    console.log("I have enough resources, making you a coffee!");
+    water -= coffee[0];
+    milk -= coffee[1];
+    beans -= coffee[2];
+    money += coffee[3];
+    cups -= 1;
+}
+
 
 function buy() {
     console.log("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:");
-    let coffe = input();
-    switch (coffe){
+    let coffee = input();
+    switch (coffee){
         case("1"):
-            if (!(water >= 250 && beans >= 16 && cups > 0)){
-                if(water < 250)
-                    console.log("Sorry, not enough water!");
-                else if(beans < 16)
-                    console.log("Sorry, not enough beans!");
-                else if(cups < 1)
-                    console.log("Sorry, not enough cups!");
-                return;
-            }
-            console.log("I have enough resources, making you a coffee!");
-            water -= 250;
-            beans -= 16;
-            money += 4;
-            cups -= 1;
+            make(espresso);
             break;
         case("2"):
-            if (!(water >= 350 && milk >= 75 && beans >= 20 && cups > 0)){
-                if(water < 350)
-                    console.log("Sorry, not enough water!");
-                else if(milk < 75)
-                    console.log("Sorry, not enough milk!");
-                else if(beans < 20)
-                    console.log("Sorry, not enough beans!");
-                else if(cups < 1)
-                    console.log("Sorry, not enough cups!");
-                return;
-            }
-            console.log("I have enough resources, making you a coffee!");
-            water -= 350;
-            milk -= 75;
-            beans -= 20;
-            money += 7;
-            cups -= 1;
+            make(latte);
             break;
         case("3"):
-            if (!(water >= 200 && milk >= 100 && beans >= 12 && cups > 0)){
-                if(water < 200)
-                    console.log("Sorry, not enough water!");
-                else if(milk < 100)
-                    console.log("Sorry, not enough milk!");
-                else if(beans < 12)
-                    console.log("Sorry, not enough beans!");
-                else if(cups < 1)
-                    console.log("Sorry, not enough cups!");
-                return;
-            }
-            console.log("I have enough resources, making you a coffee!");
-            water -= 200;
-            milk -= 100;
-            beans -= 12;
-            money += 6;
-            cups -= 1;
+            make(cappuccino);
             break;
         case("back"):
             return;
@@ -102,26 +81,23 @@ function main() {
    while (true) {
        console.log("Write action (buy, fill, take, remaining, exit):");
        choice = input();
-       if (choice == "exit")
+       if (choice === "exit")
            break;
-       else if (["buy", "fill", "take", "remaining"].includes(choice))
-           switch (choice) {
-               case ("buy"):
-                   buy();
-                   break;
-               case("fill"):
-                   fill();
-                   break;
-               case("take"):
-                   take();
-                   break;
-               case("remaining"):
-                   printStatus();
-                   break;
-           }
+       switch (choice) {
+           case ("buy"):
+               buy();
+               break;
+           case("fill"):
+               fill();
+               break;
+           case("take"):
+               take();
+               break;
+           case("remaining"):
+               printStatus();
+               break;
+       }
    }
-
 }
 
 main();
-
